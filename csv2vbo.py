66851c6cc06677311e6edd8a26387b2a83049e23
Lubomír Bulej <bulej@d3s.mff.mcuni.cz>
2014-10-06 16:00:25 +0200

    Added filtering of potentially duplicate header lines when reading input files. Moved duplicate row filtering from output into transformation code. Moved fudging of the "satellites" channel into transformation code. Simplified VBO header calculation and channel unit collection in transformation code.

--- csv2vbo.py
+++ csv2vbo.py
@@ -96,7 +96,7 @@ class Converter(object):
 		"""Checks whether this converter recognizes the header row."""
 		return all([item in row for item in self._base_map.keys()])
 
-	def _get_vbo_name(self, csv_name):
+	def _map_name(self, csv_name):
 		result = self._base_map.get(csv_name)
 		if result is None:
 			channel = self._user_map.get(csv_name)
@@ -105,57 +105,76 @@ class Converter(object):
 
 		return result
 
-	def _map_value(self, csv_name, csv_value):
-		vbo_name = self._get_vbo_name(csv_name)
+	def _get_mapper(self, vbo_name):
 		if vbo_name is None:
-			return (None, None)
+			# No mapper is expected in this case.
+			return None
 
 		mapper = self._value_map.get(vbo_name)
-		if mapper is None:
+		if mapper is not None:
+			return (vbo_name, mapper)
+		else:
 			raise Exception(
 				"no mapper for %s in %s" % (vbo_name, type(self).__name__)
 			)
 
+	def _map_value(self, csv_value, mapper):
 		try:
-			return (vbo_name, mapper(csv_value))
+			return mapper[1](csv_value)
 
 		except Exception as ex:
+			# Warn about the problem, but return something relatively usable.
 			print(
-				"warning: failed to convert '%s' when mapping %s to %s" %
-				(csv_value, csv_name, vbo_name), file=sys.stderr
+				"warning: failed to convert '%s' to %s\nexception: %s" %
+				(csv_value, mapper[0], ex), file=sys.stderr
 			)
-			print("exception: %s" % ex, file=sys.stderr)
-			return (vbo_name, "-0")
+			return "-0"
+
+	def _map_values(self, csv_values, mappers):
+		for (csv_value, mapper) in zip(csv_values, mappers):
+			if mapper is not None:
+				yield self._map_value(csv_value, mapper)
 
 	def convert(self, csv_data):
 		"""Converts a CSV data frame to VBO data frame."""
-		vbo_head = None
-		vbo_units = {}
+		# Map names from CSV header to VBO names (or None if unsupported).
+		vbo_names = [self._map_name(name) for name in csv_data.header()]
+
+		# Get the VBO header consisting only of supported fields.
+		vbo_head = [x for x in vbo_names if x is not None]
+
+		# CircuitTools requires the VBO data to contain the "satellites"
+		# channel. Some loggers don't provide it, so we fudge it.
+		base_row = []
+		if "satellites" not in vbo_head:
+			vbo_head.insert(0, "satellites")
+			base_row.append("005")
+
+		# Collect CSV-to-VBO value mappers for supported VBO data types.
+		mappers = [self._get_mapper(name) for name in vbo_names]
+
+		# Map CSV values to VBO values row by row and eliminate potentially
+		# duplicate adjacent rows resulting from mapping a subset of fields.
 		vbo_rows = []
-
+		last_row = None
 		for csv_row in csv_data.rows():
-			# The VBO row needs to be ordered to maintain insertion order!
-			vbo_row = OrderedDict()
-			for (csv_name, csv_value) in zip(csv_data.header(), csv_row):
-				(vbo_name, vbo_value) = self._map_value(csv_name, csv_value)
-				if vbo_name is not None:
-					vbo_row[vbo_name] = vbo_value
-					# Collect units for user-defined data types.
-					if vbo_name not in vbo_units and csv_name in self._user_map:
-						vbo_units[vbo_name] = self._user_map[csv_name][1]
+			vbo_row = base_row.copy()
+			vbo_row.extend(self._map_values(csv_row, mappers))
+			if vbo_row != last_row:
+				vbo_rows.append(vbo_row)
+				last_row = vbo_row
 
-			# If there is no VBO head row yet, take it from the VBO row keys,
-			# so that it contains both base and user data types.Converter.
-			if vbo_head is None:
-				vbo_head = vbo_row.keys()
-
-			# Convert the (ordered) VBO row dictionary to a list.
-			vbo_rows.append(list(vbo_row.values()))
+		# Determine units for user-defined data types.
+		vbo_units={}
+		for csv_name in csv_data.header():
+			if csv_name in self._user_map:
+				(vbo_name, unit) = self._user_map.get(csv_name)
+				if vbo_name in vbo_head:
+					vbo_units[vbo_name] = unit
 
 		return DataFrame(
-			head=vbo_head, data=vbo_rows, info=csv_data.comments(),
-			# Attach units for user-defined data types in the data row.
-			units=vbo_units
+			head=vbo_head, data=vbo_rows,
+			info=csv_data.comments(), units=vbo_units
 		)
 
 
@@ -244,12 +263,14 @@ def read_csv(input):
 	# Filter out empty rows and strip all space around data items.
 	rows = [[x.strip() for x in row] for row in reader if len(row) > 0]
 
+	# The header row is the first row with the maximal number of items.
 	head_row = max(rows, key=len)
 
-	# Split the data at the header into info rows and data rows
+	# Split the data at the header into info rows and data rows.
+	# Eliminate potentially duplicate header rows from the data rows.
 	head_index = rows.index(head_row)
 	info_rows = rows[0:head_index]
-	data_rows = rows[head_index + 1:]
+	data_rows = [row for row in rows[head_index:] if row != head_row]
 
 	return DataFrame(head=head_row, data=data_rows, info=info_rows)
 
@@ -269,27 +290,29 @@ def find_converter(data):
 
 
 def write_vbo(vbo_data, vbo_output):
-	# Use ordered dict here to keep the usual base type order in VBO.
+	# Required base types that make a log useful.
 	base_types = OrderedDict((
 		("satellites", "sats"), ("time", "time"),
 		("latitude", "lat"), ("longitude", "long"),
+	))
+
+	# Optional base types that may be provided by a data logger.
+	opt_base_types = OrderedDict((
 		("velocity kmh", "velocity"), ("heading", "heading"),
 		("height", "height"), ("vertical velocity m/s", "vert-vel"),
 		("vertical velocity kmh", "vert-vel"),
 		("yaw rate deg/s", "yaw-calc")
 	))
 
+	# Add only optional base types provided by the converter.
+	base_types.update((
+		(k, v) for (k, v) in opt_base_types.items()
+			if k in vbo_data.header()
+	))
+
 	# Curry print() with the output file and MS-DOS line suffix.
 	output = partial(print, file=vbo_output, end="\r\n")
-
 	output(datetime.now().strftime("File created on %d/%m/%Y at %I:%M:%S %p"))
-
-	# Keep only base types provided by the converter.
-	# Preserve the "satellites" column, because it is mandatory.
-	base_types = OrderedDict((
-		(k, v) for (k, v) in base_types.items()
-			if k in vbo_data.header() or k == "satellites"
-	))
 
 	output("\r\n[header]")
 	for base_type in base_types.keys():
@@ -306,9 +329,9 @@ def write_vbo(vbo_data, vbo_output):
 		for user_type in user_types:
 			output(vbo_data.units().get(user_type))
 
-	# Output comments in the comments section. Single value rows become comment
-	# lines. For multi-value rows, the first item becomes a label, and the
-	# remaining items are output separated by a semicolon, if non-empty.
+	# Output comments into the comments section. Single value rows become
+	# comment lines. For multi-value rows, the first item becomes a label, and
+	# the remaining items, if non-empty, are output separated by a semicolon.
 	output("\r\n[comments]")
 	for row in vbo_data.comments():
 		if len(row) == 1:
@@ -321,27 +344,15 @@ def write_vbo(vbo_data, vbo_output):
 	output("\r\n[column names]")
 	output(" ".join(list(base_types.values()) + user_types))
 
-	# Get a map of indices to output data in the usual VBO order.
+	# Determine the output order of VBO data types and get a map of
+	# indices for getting the appropriate column from a VBO data row.
+	out_order = list(base_types.keys()) + user_types
 	indices = dict([(n, i) for (i, n) in enumerate(vbo_data.header())])
 
-	# If the convertor did not provide "satellites", fudge it.
-	if "satellites" in vbo_data.header():
-		vbo_rows = vbo_data.rows()
-	else:
-		indices["satellites"] = len(vbo_data.header())
-		vbo_rows = map(lambda r: list(r) + ["005"], vbo_data.rows())
-
-	# Determine the order output data values
-	out_order = list(base_types.keys()) + user_types
-
 	output("\r\n[data]")
-	last_row = None
-	for row in vbo_rows:
+	for row in vbo_data.rows():
 		out_row = [row[indices.get(name)] for name in out_order]
-		# Filter-out consecutive duplicates.
-		if out_row != last_row:
-			output(" ".join(out_row))
-			last_row = out_row
+		output(" ".join(out_row))
 
 
 ### Read the CSV input, find a suitable converter,
